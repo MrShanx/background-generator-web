d6f841b4b58bc208d2aaeeea243a3b019ee14284
Emmanuel Talan <36684257+MrShanx@users.noreply.github.com>
2018-04-25 18:25:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var colorleft = document.querySelector(".color1");
+var colorright = document.querySelector(".color2");
+var h3 = document.querySelector("h3");
+var body = document.getElementById("gradient");
+
+function setString() {
+	var colorStr = "linear-gradient(to right, " 
+		+ colorleft.value 
+		+ ", " 
+		+ colorright.value 
+		+ ")";
+
+	h3.innerHTML = colorStr;
+	return colorStr;
+}
+function changeGradient() {
+	body.style.background = setString();
+}
+
+colorleft.addEventListener("input", changeGradient);
+colorright.addEventListener("input", changeGradient);
+
+setString();
+
