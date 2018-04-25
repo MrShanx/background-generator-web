443369b5bd765b140d5b30bef01d832473f9d501
Emmanuel Talan <36684257+MrShanx@users.noreply.github.com>
2018-04-25 18:51:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ var h3 = document.querySelector("h3");
 var body = document.getElementById("gradient");
 
 function setString() {
-	h3.textContent = body.style.background + ";";
-}
-function changeGradient() {
 	var colorStr = "linear-gradient(to right, " 
 		+ colorleft.value 
 		+ ", " 
 		+ colorright.value 
 		+ ")";
-	body.style.background = colorStr;
-	setString();
+	h3.textContent = colorStr + ";";
+	return colorStr;
+}
+function changeGradient() {
+	body.style.background = setString();
 }
 
 colorleft.addEventListener("input", changeGradient);
