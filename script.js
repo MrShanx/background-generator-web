4738c5b15e0d4d7c7053968c03dce3407091a0b9
Emmanuel Talan <36684257+MrShanx@users.noreply.github.com>
2018-04-25 19:37:48 -0400

    Add files via upload
    updated with randomizer functionality

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function setString() {
 		+ ", " 
 		+ colorright.value 
 		+ ")";
+
 	h3.textContent = colorStr + ";";
 	return colorStr;
 }
@@ -19,6 +20,26 @@ function changeGradient() {
 
 colorleft.addEventListener("input", changeGradient);
 colorright.addEventListener("input", changeGradient);
-
 setString();
+
+
+//randomizer
+var random = document.getElementById("random");
+random.addEventListener("click", randomGradient);
+
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function randomGradient() {
+    colorleft.value = getRandomColor();
+	colorright.value = getRandomColor();
+	changeGradient();
+}
+
 
